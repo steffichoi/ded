2a2fef3c3b7f6ea14cee5b7d8aa1727d2eae50e6
steffichoi <steffic.06@gmail.com>
2015-12-10 20:05:30 -0500

    f

--- router/sr_router.c
+++ router/sr_router.c
@@ -258,7 +258,7 @@ void sr_sendICMP(struct sr_instance *sr, uint8_t *buf, unsigned int len, uint8_t
     uint8_t *icmpPacket;
     icmpPacket = createICMP(type, code, packet, len);
 
-    memcpy(icmpHeader, icmpPacket, sizeof(sr_icmp_t3_hdr_t *))
+    memcpy(icmpHeader, icmpPacket, sizeof(sr_icmp_t3_hdr_t *)+len);
     memcpy(ethHeader->ether_shost,iface->addr,6);
     ethHeader->ether_type = htons(0x0800);
     if (ip_src == 0){

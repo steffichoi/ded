a85ebcad9d32a680092cec4ee5644d055546e70d
steffichoi <steffic.06@gmail.com>
2015-12-11 13:56:07 -0500

    yay

--- router/sr_arpcache.h
+++ router/sr_arpcache.h
@@ -148,6 +148,7 @@ int   sr_arpcache_destroy(struct sr_arpcache *cache);
 void *sr_arpcache_timeout(void *cache_ptr);
 
 /* Helper function to handle ARP requests */
-void handle_arpreq(struct sr_instance *sr, struct sr_arpreq *req, char *iface);
+void handle_arpreq(struct sr_instance *sr, struct sr_arpreq *req);
+void send_request(struct sr_instance* sr, uint32_t ip);
 
 #endif

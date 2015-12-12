420903bb955678615d5d542ada77b88e22f594ef
steffichoi <steffic.06@gmail.com>
2015-12-11 21:07:32 -0500

    yay

--- router/sr_nat.h
+++ router/sr_nat.h
@@ -107,10 +107,10 @@ struct sr_nat_mapping *sr_nat_lookup_internal(struct sr_nat *nat,
 /* Insert a new mapping into the nat's mapping table.
    You must free the returned structure if it is not NULL. */
 struct sr_nat_mapping *sr_nat_insert_mapping(struct sr_nat *nat,
-  uint32_t ip_int, uint16_t aux_int, sr_nat_mapping_type type );
+  uint32_t ip_int, uint16_t aux_int, sr_nat_mapping_type type);
 
 struct sr_nat_mapping *sr_nat_insert_mapping_unsol(struct sr_nat *nat,
-  uint16_t aux_ext, sr_nat_mapping_type type )
+  uint16_t aux_ext, sr_nat_mapping_type type);
 
 void sr_nat_refresh_mapping(struct sr_nat *nat, struct sr_nat_mapping *copy);
 
